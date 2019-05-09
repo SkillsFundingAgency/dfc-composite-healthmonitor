48287cbe578a3ba03334ce017b930faf9581a30b
Ilyas Dhin <ilyasdhin@gmail.com>
2019-05-09 14:16:53 +0100

    added health service

---
+++ DFC.Composite.HealthMonitor/DFC.Composite.HealthMonitor/Services/IHealthService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFC.Composite.HealthMonitor.Services
+{
+    public interface IHealthService
+    {
+        Task<bool> IsHealthy(string url);
+    }
+}
